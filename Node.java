72430281507b667ca7fee19c02f1a545c0665f84
qiqimonkey4300 <yueqi04@gmail.com>
2021-01-06 12:09:21 -0500

    constructor added

---
+++ Node.java
@@ -0,0 +1,11 @@
+public class Node {
+  private String data;
+
+  private Node next, prev;
+
+  public Node(String value){
+    data = value;
+  }
+  //write get/set methods for all three instance variables.
+  
+}
