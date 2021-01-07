729569603b733355e081b27439f590ec09bea4a1
qiqimonkey4300 <yueqi04@gmail.com>
2021-01-07 17:27:37 -0500

    included the two add methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,4 +1,4 @@
-public class MyLinkedList{
+public class MyLinkedList {
   private int size;
   private Node start, end;
 
@@ -9,19 +9,51 @@ public class MyLinkedList{
   public int size() {
     return size;
   }
-  /*
-  public boolean add(String value) { }
 
-  public void add(int index, String value) { }
+  public boolean add(String value) {
+    Node newest = new Node(value);
+    if (size == 0) {
+      start = newest;
+    } else {
+      newest.setPrev(end); end.setNext(newest);
+    }
+    end = newest;
+    size++;
+    return true;
+  }
 
-  public String get(int index) {}
+  public void add(int index, String value) {
+    if (index < 0 || index > size) {
+      throw new IndexOutOfBoundsException("This index isn't allowed");
+    }
+    if (size == 0 || index == size) {
+      add(value);
+    } else {
+      Node newest = new Node(value);
+      if (index == 0) {
+        newest.setPrev(null);
+        newest.setNext(start);
+        start.setPrev(newest);
+        start = newest;
+      }
+      else {
+        newest.setPrev(getNode(index - 1));
+        newest.setNext(getNode(index));
+        getNode(index - 1).setNext(newest);
+        getNode(index + 1).setPrev(newest);
+      }
+      size++;
+    }
+  }
 
-  public String set(int index, String value) { }
+  //public String get(int index) {}
 
-  public String toString() { }
+  //public String set(int index, String value) { }
 
-  public String toStringReversed() { }
-  */
+  //public String toString() {}
+
+  //public String toStringReversed() { }
+
   //Any helper method that returns a Node object MUST BE PRIVATE!
   private Node getNode(int index) {
     Node current = start;
